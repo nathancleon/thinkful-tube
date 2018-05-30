73240f0be9998b46c2dcddc2ca2f61cfa4336c68
Nathan <nathancleon@gmail.com>
2018-05-30 16:47:52 -0400

    added normalize css and set project outline

---
+++ main.js
@@ -0,0 +1,22 @@
+const YT_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
+const API_KEY = "AIzaSyA1t6WOmG1OcLBTdSqCJAWRAokIEj0jmyM";
+const YT_WATCH_URL = "https://www.youtube.com/watch?v=";
+
+
+function dataRequest(query) {
+    //set parameters and ajax request, call displayResults() from within ajax request function
+}
+
+function displayResults() {
+    //Appends data to js-results div in DOM
+    //use for each? to iterate through
+    //store values for img ref and a ref
+    //generate string literal with thumbnail img wrapped in a link (<a>)
+    
+}
+
+function watchSubmit() {
+    //triggers actions when submit button is clicked
+    //need text input value saved
+    //need to clear results each time submit button is clicked
+}
