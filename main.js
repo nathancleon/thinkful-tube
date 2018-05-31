45f465e89755c998d804f1849638f7ea6bd0f3c4
Nathan <nathancleon@gmail.com>
2018-05-31 10:51:02 -0400

    added pagination feature

--- main.js
+++ main.js
@@ -2,15 +2,33 @@ const YT_SEARCH_URL = "https://www.googleapis.com/youtube/v3/search";
 const API_KEY = "AIzaSyA1t6WOmG1OcLBTdSqCJAWRAokIEj0jmyM";
 const YT_WATCH_URL = "https://www.youtube.com/watch?v=";
 
+//declaring global variables to pass values into with the functions to store between requests
+let QUERY;
+let nextPageToken = "";
+let prevPageToken = "";
 
-function dataRequest(query) {
+function dataRequest(query, pageToken) {
     //set parameters and ajax request, call displayResults() from within ajax request function
     let parameters = {
         part: 'id, snippet',
         key: API_KEY,
         q: query,
+        pageToken: pageToken,
     }
     $.getJSON(YT_SEARCH_URL, parameters, function(data){
+        console.log(data);
+
+        //if data has nextPageToken, show the button and store value
+        if(data.nextPageToken) {
+            $('.btn-next').show();
+            nextPageToken = data.nextPageToken;
+        }
+
+        //if data has prevPageToken, show the button and store value
+        if(data.prevPageToken) {
+            $('.btn-prev').show();
+            prevPageToken = data.prevPageToken;
+        }
         displayResults(data.items);
     });
 }
@@ -20,12 +38,16 @@ function displayResults(results) {
     //use for each to iterate through
     //store values for img ref and a ref
     //generate string literal with thumbnail img wrapped in a link (<a>)
+
+    //empty html string that will be updated in the for each function
     let resultsHTML = "";
     $.each(results, function(index, value){
-            let thumbnailLink = YT_WATCH_URL + value.id.videoId;
-            let thumbnailImg = value.snippet.thumbnails.default.url;
-            let thumbnailAlt = value.snippet.title;
-            resultsHTML += `<div class="js-thumbnail"><a href="${thumbnailLink}" role="link"><img src="${thumbnailImg}" title="${thumbnailAlt}"></a></div>`; 
+        let thumbnailLink = YT_WATCH_URL + value.id.videoId;
+        let thumbnailImg = value.snippet.thumbnails.default.url;
+        let thumbnailAlt = value.snippet.title;
+        resultsHTML += `<div class="js-thumbnail"><a href="${thumbnailLink}" 
+        role="link"><img src="${thumbnailImg}" 
+        title="${thumbnailAlt}"></a></div>`; 
     });
     $('.js-results').html(resultsHTML); ;
 }
@@ -37,7 +59,17 @@ function watchSubmit() {
     $('#js-search-form').submit(function(event) {
         event.preventDefault();
         let query = $(event.currentTarget).find('input[name="search-query"]').val().trim();
-        dataRequest(query);
+        QUERY = query;
+        dataRequest(query, "");
+    });
+
+    //triggers dataRequest function when clicked
+    $('.btn-next').click(function(event) {
+        dataRequest(QUERY, nextPageToken);
+    });
+
+    $('.btn-prev').click(function(event) {
+        dataRequest(QUERY, prevPageToken);
     });
 }
 
